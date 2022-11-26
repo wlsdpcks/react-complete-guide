11e05301b9e69ced6a3b015705e7f2c2c35a9ee3
wlsdpcks <80802907+wlsdpcks@users.noreply.github.com>
2022-11-26 11:01:31 +0900

    69. 데이터의 렌더링 목록

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -1,48 +1,20 @@
 import React, { useState } from "react";
 import "./ExpenseForm.css";
 const ExpenseForm = () => {
-  // 1. useState를 독립해서 쓰는 방법
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
 
-  // 2. useState를 통합해서 쓰는 방법
-  // const [userInput, setUserInput] = useState({
-  //   setEnteredTitle: "",
-  //   setEnteredAmount: "",
-  //   setEnteredDate: "",
-  // });
-
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
-
-    // 2.
-    // setUserInput({
-    //   ...userInput,
-    //   enteredTitle: event.target.value,
-    // });
-
-    // setUserInput((prevState) => {
-    //   return { ...prevState, enteredTitle: event.target.value };
-    // });
   };
+
   const amountChangeHandler = (event) => {
     setEnteredAmount(event.target.value);
-
-    // 2.
-    // setUserInput({
-    //   ...userInput,
-    //   setEnteredAmount: event.target.value,
-    // });
   };
+
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
-
-    // 2.
-    // setUserInput({
-    //   ...userInput,
-    //   setEnteredDate: event.target.value,
-    // });
   };
 
   const submitHandler = (event) => {
