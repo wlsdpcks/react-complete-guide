08a42f98dcd241fde8fb2393d9c79fdc66fae3c6
wlsdpcks <80802907+wlsdpcks@users.noreply.github.com>
2022-12-03 15:35:40 +0900

    80. 마무리 및 다음 단계

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,13 +3,16 @@ import ChartBar from "./ChartBar";
 import "./Chart.css";
 
 const Chart = (props) => {
+  const dataPointsValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const totalmaximum = Math.max(...dataPointsValues);
+
   return (
     <div className="chart">
       {props.dataPoints.map((dataPoint) => (
         <ChartBar
           key={dataPoint.label}
           value={dataPoint.value}
-          maxValue={null}
+          maxValue={totalmaximum}
           label={dataPoint.label}
         />
       ))}
